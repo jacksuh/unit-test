d6c32043b720368e88cfb644774fb26f971f658b
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-03-09 13:07:38 -0300

    AssertEquals

--- src/test/java/br/jackson/servicos/AssertTest.java
+++ src/test/java/br/jackson/servicos/AssertTest.java
@@ -3,6 +3,8 @@ package br.jackson.servicos;
 import org.junit.Assert;
 import org.junit.Test;
 
+import br.jackson.entidades.Usuario;
+
 public class AssertTest {
 
 	@Test
@@ -11,7 +13,33 @@ public class AssertTest {
 		Assert.assertTrue(true);
 		Assert.assertFalse(false);
 		
-		Assert.assertEquals(1, 1);
+		Assert.assertEquals("Erro de comparacao",1, 1);
 		Assert.assertEquals(0.51, 0.51, 0.01);
+		Assert.assertEquals(Math.PI, 3.14, 0.01);
+		
+		int i = 5;
+		Integer i2 = 5;
+		Assert.assertEquals(Integer.valueOf(i), i2);
+		Assert.assertEquals(i, i2.intValue());
+		
+		//validação com String
+		Assert.assertEquals("bola", "bola");
+		Assert.assertNotEquals("bola", "casa");
+		Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+		Assert.assertTrue("bola".startsWith("bo"));
+		
+		
+		//coparacao de objetos
+		Usuario u1 = new Usuario("Usuario 1");
+		Usuario u2 = new Usuario("Usuario 1");
+		Usuario u3 = null;
+		
+		Assert.assertEquals(u1, u2);
+		
+		Assert.assertSame(u2, u2);
+		Assert.assertNotSame(u1, u2);
+		
+		Assert.assertNull(u3);
+		Assert.assertNotNull(u2);
 	}
 }
