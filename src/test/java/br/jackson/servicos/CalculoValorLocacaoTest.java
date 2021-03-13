87c314be883c941bfd437b478c7c93a09fff4952
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-03-13 12:19:32 -0300

    Testes Parametrizaveis

--- src/test/java/br/jackson/servicos/CalculoValorLocacaoTest.java
+++ src/test/java/br/jackson/servicos/CalculoValorLocacaoTest.java
@@ -1,6 +1,7 @@
 package br.jackson.servicos;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.hamcrest.CoreMatchers;
@@ -9,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import br.jackson.entidades.Filme;
 import br.jackson.entidades.Locacao;
@@ -30,6 +32,23 @@ public class CalculoValorLocacaoTest {
 		service = new LocacaoService();
 
 	}
+	
+	private static Filme filme1 = new Filme("Filme 1", 2, 4.0);
+	private static Filme filme2 = new Filme("Filme 2", 2, 4.0);
+	private static Filme filme3 = new Filme("Filme 3", 2, 4.0);
+	private static Filme filme4 = new Filme("Filme 4", 2, 4.0);
+	private static Filme filme5 = new Filme("Filme 5", 2, 4.0);
+	private static Filme filme6 = new Filme("Filme 6", 2, 4.0);
+	
+	@Parameters
+	public static Collection<Object[]> getParametros(){
+		return Arrays.asList(new Object[][] {
+				{Arrays.asList(filme1, filme2, filme3), 11.0},
+				{Arrays.asList(filme1, filme2, filme3, filme4), 13.0},
+				{Arrays.asList(filme1, filme2, filme3, filme4, filme5), 14.0},
+				{Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6), 14.0}
+			});
+		}
 
 	
 	@Test
