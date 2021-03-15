043a2d23e1e0f0e01919862d7ab730052f3e0efb
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-03-15 10:22:46 -0300

    Suite de teste

---
+++ src/test/java/br/jackson/suites/SuiteExecucao.java
@@ -0,0 +1,20 @@
+package br.jackson.suites;
+
+import org.junit.runner.RunWith;
+
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.jackson.servicos.CalculadoraTest;
+import br.jackson.servicos.CalculoValorLocacaoTest;
+import br.jackson.servicos.LocacaoServiceTest;
+
+//@RunWith(Suite.class)
+@SuiteClasses({
+	CalculadoraTest.class,
+	CalculoValorLocacaoTest.class,
+	LocacaoServiceTest.class
+})
+public class SuiteExecucao {
+	//Remova se puder!
+}
